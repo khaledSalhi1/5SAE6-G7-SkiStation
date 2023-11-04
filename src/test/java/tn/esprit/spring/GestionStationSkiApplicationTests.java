558128deb799682a1c4c1b4615dc0f9096152494
khaled <khaled.salhi@esprit.com>
2023-11-04 15:20:13 +0100

    commit2

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,11 +1,19 @@
 package tn.esprit.spring;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.repositories.ICourseRepository;
+import tn.esprit.spring.services.CourseServicesImpl;
 
 @SpringBootTest
 class GestionStationSkiApplicationTests {
 
+//	@Mock
+//	ICourseRepository iCourseRepository;
+//	@InjectMocks
+//	CourseServicesImpl courseServices;
 	@Test
 	void contextLoads() {
 	}
