ab76f4a8fc1f199a29e90d090a2920558cf7a9cf
wassimderouiche <99843728+wassimderouiche@users.noreply.github.com>
2023-11-09 17:06:08 +0100

    Create CourseServicesTest.java

---
+++ src/test/java/tn/esprit/spring/iche/CourseServicesTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.repositories.ICourseRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class CourseServicesTest {
+
+    @Mock
+    private ICourseRepository courseRepository;
+
+    @InjectMocks
+    private CourseServicesImpl courseServices;
+
+    @Test
+    public void testRetrieveAllCourses() {
+        // Arrange
+        List<Course> courses = new ArrayList<>();
+        courses.add(new Course(1L, 1, TypeCourse.TYPE_A, Support.SUPPORT_A, 100.0f, 2, null));
+        courses.add(new Course(2L, 2, TypeCourse.TYPE_B, Support.SUPPORT_B, 150.0f, 3, null));
+
+        Mockito.when(courseRepository.findAll()).thenReturn(courses);
+
+        // Act
+        List<Course> result = courseServices.retrieveAllCourses();
+
+        // Assert
+        assertEquals(courses, result);
+    }
+
+    @Test
+    public void testAddCourse() {
+        // Arrange
+        Course newCourse = new Course(3L, 3, TypeCourse.TYPE_C, Support.SUPPORT_C, 200.0f, 4, null);
+
+        Mockito.when(courseRepository.save(newCourse)).thenReturn(newCourse);
+
+        // Act
+        Course result = courseServices.addCourse(newCourse);
+
+        // Assert
+        assertEquals(newCourse, result);
+    }
+
+    @Test
+    public void testRetrieveCourse() {
+        // Arrange
+        Long courseId = 1L;
+        Course course = new Course(courseId, 1, TypeCourse.TYPE_A, Support.SUPPORT_A, 100.0f, 2, null);
+
+        Mockito.when(courseRepository.findById(courseId)).thenReturn(Optional.of(course));
+
+        // Act
+        Course result = courseServices.retrieveCourse(courseId);
+
+        // Assert
+        assertEquals(course, result);
+    }
+}
